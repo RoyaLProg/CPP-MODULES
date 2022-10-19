0a7de4de02b7983e244c8b0de9cc95eb5503fe33
Camille CAMBIUM <ccambium@e1r1p8.clusters.42paris.fr>
2022-10-19 14:11:13 +0200

    initialisation

---
+++ cpp00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 14:08:09 by ccambium          #+#    #+#             */
+/*   Updated: 2022/10/17 15:09:11 by ccambium         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+void	megaphone(std::string argv[])
+{
+	for (size_t i = 1; !argv[i].empty(); i++)
+	{
+		std::transform(argv[i].begin(), argv[i].end(), argv[i].begin(), ::toupper);
+		std::cout << argv[i];		
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+	std::string s[argc + 1];
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (EXIT_SUCCESS);
+	}
+	for (int i = 0; i < argc; i++)
+		s[i].assign(argv[i]);
+	s[argc] = "";
+	megaphone(s);
+}
