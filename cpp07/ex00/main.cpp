c357ff1761ddb69c20bc30f1d5b355e7b8ef7eaf
Camille CAMBIUM <ccambium@student.42.fr>
2023-01-08 15:57:47 +0100

    cpp07 ex00

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 16:34:46 by ccambium          #+#    #+#             */
+/*   Updated: 2023/01/07 16:37:28 by ccambium         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "max.hpp"
+#include "min.hpp"
+#include "swap.hpp"
+#include <iostream>
+
+int main( void ) 
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
